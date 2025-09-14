4df645eb7443361a4d30889548ec154f0d2a1c60
VictorPko73 <152915385+VictorPko73@users.noreply.github.com>
2025-09-14 12:23:40 +0000

    Primer Commit

--- src/models.py
+++ src/models.py
@@ -1,19 +1,51 @@
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    name: Mapped[str] = mapped_column(String(40), unique=True, nullable=False)
+    first_name: Mapped[str] = mapped_column(
+        String(40), unique=True, nullable=False)
+    last_name: Mapped[str] = mapped_column(
+        String(40), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
 
+    # RELACIONES ENTRE TABLAS
+    followers = db.relationship(
+        'Follower',
+        foreign_keys='Follower.user_to_id',
+        backref='followed',
+        lazy='True'
+    )
+    following = db.relationship(
+        'Follower',
+        foreign_keys='Follower.user_from_id',
+        backref='follower',
+        lazy='True'
+    )
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+
+class Follower(db.Model):
+    user_from_id: Mapped[int] = mapped_column(
+        db.ForeignKey('user.id'), nullable=False)
+    user_to_id: Mapped[int] = mapped_column(
+        db.ForeignKey('user.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "user_from_id": self.user_from_id,
+            "user_to_id": self.user_to_id,
         }
